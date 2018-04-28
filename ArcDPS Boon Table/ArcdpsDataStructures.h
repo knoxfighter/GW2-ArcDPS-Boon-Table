2ab25cf3bd764f3b7e8447ddfc5d1beac900bfaa
Marty Rand <marsedge95@gmail.com>
2018-04-28 15:09:44 -0500

    Add arc enums

--- ArcDPS Boon Table/ArcdpsDataStructures.h
+++ ArcDPS Boon Table/ArcdpsDataStructures.h
@@ -1,6 +1,83 @@
 #pragma once
 #include <inttypes.h>
 
+//enums and structs:
+/* iff */
+enum iff {
+	IFF_FRIEND, // green vs green, red vs red
+	IFF_FOE, // green vs red
+	IFF_UNKNOWN // something very wrong happened
+};
+
+/* combat result (physical) */
+enum cbtresult {
+	CBTR_NORMAL, // good physical hit
+	CBTR_CRIT, // physical hit was crit
+	CBTR_GLANCE, // physical hit was glance
+	CBTR_BLOCK, // physical hit was blocked eg. mesmer shield 4
+	CBTR_EVADE, // physical hit was evaded, eg. dodge or mesmer sword 2
+	CBTR_INTERRUPT, // physical hit interrupted something
+	CBTR_ABSORB, // physical hit was "invlun" or absorbed eg. guardian elite
+	CBTR_BLIND, // physical hit missed
+	CBTR_KILLINGBLOW // physical hit was killing hit
+};
+
+/* combat activation */
+enum cbtactivation {
+	ACTV_NONE, // not used - not this kind of event
+	ACTV_NORMAL, // activation without quickness
+	ACTV_QUICKNESS, // activation with quickness
+	ACTV_CANCEL_FIRE, // cancel with reaching channel time
+	ACTV_CANCEL_CANCEL, // cancel without reaching channel time
+	ACTV_RESET // animation completed fully
+};
+
+/* combat state change */
+enum cbtstatechange {
+	CBTS_NONE, // not used - not this kind of event
+	CBTS_ENTERCOMBAT, // src_agent entered combat, dst_agent is subgroup
+	CBTS_EXITCOMBAT, // src_agent left combat
+	CBTS_CHANGEUP, // src_agent is now alive
+	CBTS_CHANGEDEAD, // src_agent is now dead
+	CBTS_CHANGEDOWN, // src_agent is now downed
+	CBTS_SPAWN, // src_agent is now in game tracking range
+	CBTS_DESPAWN, // src_agent is no longer being tracked
+	CBTS_HEALTHUPDATE, // src_agent has reached a health marker. dst_agent = percent * 10000 (eg. 99.5% will be 9950)
+	CBTS_LOGSTART, // log start. value = server unix timestamp **uint32**. buff_dmg = local unix timestamp. src_agent = 0x637261 (arcdps id)
+	CBTS_LOGEND, // log end. value = server unix timestamp **uint32**. buff_dmg = local unix timestamp. src_agent = 0x637261 (arcdps id)
+	CBTS_WEAPSWAP, // src_agent swapped weapon set. dst_agent = current set id (0/1 water, 4/5 land)
+	CBTS_MAXHEALTHUPDATE, // src_agent has had it's maximum health changed. dst_agent = new max health
+	CBTS_POINTOFVIEW, // src_agent will be agent of "recording" player
+	CBTS_LANGUAGE, // src_agent will be text language
+	CBTS_GWBUILD, // src_agent will be game build
+	CBTS_SHARDID, // src_agent will be sever shard id
+	CBTS_REWARD, // src_agent is self, dst_agent is reward id, value is reward type. these are the wiggly boxes that you get
+	CBTS_BUFFINITIAL // combat event that will appear once per buff per agent on logging start (zero duration, buff==18)
+};
+
+/* combat buff remove type */
+enum cbtbuffremove {
+	CBTB_NONE, // not used - not this kind of event
+	CBTB_ALL, // all stacks removed
+	CBTB_SINGLE, // single stack removed. disabled on server trigger, will happen for each stack on cleanse
+	CBTB_MANUAL, // autoremoved by ooc or allstack (ignore for strip/cleanse calc, use for in/out volume)
+};
+
+/* custom skill ids */
+enum cbtcustomskill {
+	CSK_RESURRECT = 1066, // not custom but important and unnamed
+	CSK_BANDAGE = 1175, // personal healing only
+	CSK_DODGE = 65001 // will occur in is_activation==normal event
+};
+
+/* language */
+enum gwlanguage {
+	GWL_ENG = 0,
+	GWL_FRE = 2,
+	GWL_GEM = 3,
+	GWL_SPA = 4,
+};
+
 /* arcdps export table */
 typedef struct arcdps_exports {
 	uintptr_t size; /* arcdps internal use, ignore */
