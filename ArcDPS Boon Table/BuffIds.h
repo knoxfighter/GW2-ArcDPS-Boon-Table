c1d73a08da6be18eaa4fb3a171f130bbdb8aee7c
Marty Rand <marsedge95@gmail.com>
2018-12-21 14:17:13 -0600

    Put the buff ids in the definitions instead of as a bunch of constants

--- ArcDPS Boon Table/BuffIds.h
+++ ArcDPS Boon Table/BuffIds.h
@@ -4,36 +4,6 @@
 #include <list>
 #include <string>
 
-const uint32_t BUFF_MIGHT = 740;
-const uint32_t BUFF_FURY = 725;
-const uint32_t BUFF_REGEN = 718;
-const uint32_t BUFF_PROT = 717;
-const uint32_t BUFF_VIGOR = 726;
-const uint32_t BUFF_QUICK = 1187;
-const uint32_t BUFF_ALAC = 30328;
-const uint32_t BUFF_RETAL = 873;
-const uint32_t BUFF_STAB = 1122;
-const uint32_t BUFF_AEGIS = 743;
-const uint32_t BUFF_SWIFT = 719;
-const uint32_t BUFF_RESIST = 26980;
-const uint32_t BUFF_EA = 14222;
-const uint32_t BUFF_PINPOINT = 38333;
-const uint32_t BUFF_ASSASSINS_PRESENCE = 26854;
-const uint32_t BUFF_SPOTTER = 14055;
-const uint32_t BUFF_SOOTHING_MIST = 5587;
-const uint32_t BUFF_VAMPIRIC_AURA = 30285;
-const uint32_t BUFF_STRENGTH_IN_NUMBERS = 13796;
-const uint32_t BUFF_BANNER_STR = 14417;
-const uint32_t BUFF_BANNER_DIS = 14449;
-const uint32_t BUFF_BANNER_TAC = 14450;
-const uint32_t BUFF_BANNER_DEF = 14543;
-const uint32_t BUFF_GLYPH_EMPOW = 31803;
-const uint32_t BUFF_SPIRIT_FROST = 50421;
-const uint32_t BUFF_SPIRIT_SUN = 50413;
-const uint32_t BUFF_SPIRIT_STONE = 50415;
-const uint32_t BUFF_SPIRIT_STORM = 50381;
-const uint32_t BUFF_SPIRIT_WATER = 50386;
-
 enum BoonType
 {
 	BoonType_boon,
